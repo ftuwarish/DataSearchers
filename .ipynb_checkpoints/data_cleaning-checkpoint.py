0458250075ae5ddf89f46b4475101e569978f1eb
unknown <aurielgue@gmail.com>
2024-12-07 22:33:23 -0800

    Added changes from previous branches for organization. Added high level question number 2, additional high level question, and changes to data_cleaning.py

--- .ipynb_checkpoints/data_cleaning-checkpoint.py
+++ .ipynb_checkpoints/data_cleaning-checkpoint.py
@@ -7,9 +7,6 @@ df['Used/New'] = df['Used/New'].replace(['Certified Pre-Owned', 'Certified Used'
 df = df[df['Drivetrain'] != '-']
 df['ConsumerRating'].fillna(df['ConsumerRating'].mean(), inplace=True)
 df = df.drop_duplicates()
-cleaned_states = ['AK', 'AL', 'AR', 'AZ', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'IA', 'ID', 'IL', 'IN', 
-                'KS', 'KY', 'LA', 'MD', 'MA', 'ME', 'MI', 'MN', 'MO', 'MS', 'MT', 'NC', 'ND', 'NE', 'NH', 'NJ', 
-                'NM', 'NV', 'NY', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VA', 'WA', 'WV', 'WI', 'WY']
 df.to_csv('Cleaned_Online_Cars_Sale_Marketplace.csv', index=False)
 print("Data cleaning complete. Cleaned dataset saved as 'Cleaned_Online_Cars_Sale_Marketplace.csv'")
 
